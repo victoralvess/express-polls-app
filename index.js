3c79fc4b57e62c7176cc34700682abdae8fb09cb
Victor Alves <vsilva.programador@gmail.com>
2018-04-13 18:42:34 -0300

    Configure and initialize passport

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 require('dotenv').config()
 
 const express = require('express');
-
+const passport = require('passport');
+const cookieSession = require('cookie-session');
 
 const passportConfig = require('./config/passport');
 const authRoutes = require('./routes/auth');
 
 const app = express();
-
+app.use(cookieSession({
+	maxAge: 24 * 60 * 60 * 1000, // 1 day
+	keys: [process.env.COOKIE_KEY]
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use('/auth', authRoutes);
 
 app.listen(process.env.PORT, () => {
