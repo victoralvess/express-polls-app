99a4d0f20d3d09dbce5a48bde77d916f60a67393
Victor Alves <vsilva.programador@gmail.com>
2018-04-13 19:00:05 -0300

    Add logout route

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
 const passport = require('passport');
 
+router.get('/logout', (req, res) => {
+	req.logout();
+	res.send('Logged Out');
+});
+
 router.get(
 	'/google',
 	passport.authenticate('google', {
