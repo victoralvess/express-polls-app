b9325a84892519297c71ef6412d7f89a4b411609
Victor Alves <vsilva.programador@gmail.com>
2018-04-25 14:11:29 -0300

    Remove unused code

--- models/question.js
+++ models/question.js
@@ -1,11 +1,6 @@
 const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
-const choicesSchema = new Schema({
-  choice_text: {type: String, required: true},
-  votes: {type: Number, default: 0}
-});
-
 const schema = new Schema({
   question: {type: String, required: true},
   choices: {type: [Schema.Types.ObjectId], required: true},
