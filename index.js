4b7b4147717b14741618dc09386cbf3e827a36a7
Victor Alves <vsilva.programador@gmail.com>
2018-04-25 17:32:16 -0300

    Add question view

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-require('dotenv').config()
-  
+require('dotenv').config();
+
 const express = require('express');
 const passport = require('passport');
 const cookieSession = require('cookie-session');
@@ -9,6 +9,7 @@ const passportConfig = require('./config/passport');
 const authRoutes = require('./routes/auth');
 
 const Question = require('./models/question');
+const Choice = require('./models/choice');
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -20,18 +21,34 @@ app.use(express.static('node_modules/bootstrap/dist'));
 app.use(express.static('node_modules/popper.js/dist/umd'));
 app.use(express.static('node_modules/jquery/dist'));
 
-app.use(cookieSession({
-	maxAge: 24 * 60 * 60 * 1000, // 1 day
-	keys: [process.env.COOKIE_KEY]
-}));
+app.use(
+  cookieSession({
+    maxAge: 24 * 60 * 60 * 1000, // 1 day
+    keys: [process.env.COOKIE_KEY],
+  }),
+);
 app.use(passport.initialize());
 app.use(passport.session());
 app.use('/auth', authRoutes);
 
 app.get('/', async (req, res) => {
-  res.render('index', { user: req.user, questions: await Question.find() });
+  res.render('index', {user: req.user, questions: await Question.find()});
+});
+
+app.get('/questions/:id', async (req, res) => {
+  try {
+    const question = await Question.findById(req.params.id);
+    question.choices = await question.getChoices();
+    res.render('question', {
+      user: req.user,
+      question,
+    });
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
 });
 
 app.listen(process.env.PORT, () => {
-	console.log(`Server running at: http://localhost:${process.env.PORT}`);
+  console.log(`Server running at: http://localhost:${process.env.PORT}`);
 });
