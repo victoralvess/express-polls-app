cb6d4fc519ff6f5f97c147ae5d11b656600430c5
Victor Alves <vsilva.programador@gmail.com>
2018-04-25 17:30:07 -0300

    Update models

--- models/choice.js
+++ models/choice.js
@@ -4,7 +4,7 @@ const {Schema} = mongoose;
 const schema = new Schema({
   choice_text: {type: String, required: true},
   votes: {type: Number, default: 0},
-  question: {type: Schema.Types.ObjectId, required: true}
+  question_id: {type: Schema.Types.ObjectId, required: true}
 });
 
 module.exports = mongoose.model('choices', schema);
--- models/question.js
+++ models/question.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
 const {Schema} = mongoose;
+const Choice = require('./choice');
 
 const schema = new Schema({
   question: {type: String, required: true},
-  choices: {type: [Schema.Types.ObjectId], required: true},
   author: {type: Schema.Types.ObjectId, required: true},
 });
 
-let Question = mongoose.model('questions', schema);
+schema.methods.getChoices = () => {
+  return Choice.find({question_id: this.id});
+};
+
+let Question = mongoose.model('question', schema);
 
 module.exports = Question;
