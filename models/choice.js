ab3324607535db42e23a40b5bed7c53a0b107bd9
Victor Alves <vsilva.programador@gmail.com>
2018-04-27 16:03:33 -0300

    Update question_id's field type to String

--- models/choice.js
+++ models/choice.js
@@ -4,7 +4,7 @@ const {Schema} = mongoose;
 const schema = new Schema({
   choice_text: {type: String, required: true},
   votes: {type: Number, default: 0},
-  question_id: {type: Schema.Types.ObjectId, required: true}
+  question_id: {type: String, required: true}
 });
 
 module.exports = mongoose.model('choices', schema);
